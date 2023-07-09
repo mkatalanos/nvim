27586c257d31f162d09c84b3c8ca4860e1a7b585
Marios Katalanos <marios@katalanos.xyz>
2023-07-09 01:07:24 +0100

    Null ls for formatting when lsp-zero doesn't work

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,11 +21,13 @@ vim.keymap.set('n', '<space>ff', ':Telescope find_files<CR>', { silent = true, r
 vim.keymap.set('n', '<space>fg', ':Telescope live_grep<CR>', { silent = true, remap = true })
 vim.keymap.set('n', '<space>fb', ':Telescope buffers<CR>', { silent = true, remap = true })
 vim.keymap.set('n', '<space>fh', ':Telescope help_tags<CR>', { silent = true, remap = true })
+vim.keymap.set('n', '<space>fm', ':Telescope keymaps<CR>', { silent = true, remap = true })
 
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', {})
 vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', {})
 vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', {})
 vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', {})
+vim.keymap.set('n', '<leader>fm', ':Telescope keymaps<CR>', {})
 vim.keymap.set('n', '<C-p>', ':Telescope git_files<CR>', {})
 
 return M
