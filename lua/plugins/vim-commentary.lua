a57549a8a57abc281b904076806b48f42f23a663
Marios Katalanos <mkatalanos@gmail.com>
2024-03-04 11:57:40 +0000

    Commentary fix

--- lua/plugins/vim-commentary.lua
+++ lua/plugins/vim-commentary.lua
@@ -1,5 +1,6 @@
 local M = { "tpope/vim-commentary" }
 
-vim.keymap.set('', '<C-_>', 'gcc', { remap = true })
+vim.keymap.set('n', '<C-_>', 'gcc', { remap = true })
+vim.keymap.set('v', '<C-_>', 'gc', { remap = true })
 
 return M
