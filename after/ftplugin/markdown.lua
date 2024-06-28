0ddf106b2b9ce39fc8390cd409bd510d86ce29cd
Marios <mkatalanos@gmail.com>
2024-06-29 01:15:13 +0300

    Created ToggleAutoWrap command

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,15 @@
-vim.opt_local.formatoptions = vim.opt_local.formatoptions + "t"
 vim.opt_local.textwidth = 80
+vim.opt_local.formatoptions = vim.opt_local.formatoptions + "t"
+
+local function toggle_auto_wrap()
+	local fo = vim.opt_local.formatoptions
+	if fo:get()["t"] then
+		print("Disabling auto-wrap")
+		vim.opt_local.formatoptions = fo - "t"
+	else
+		print("Enabling auto-wrap")
+		vim.opt_local.formatoptions = fo + "t"
+	end
+end
+
+vim.api.nvim_create_user_command("ToggleAutoWrap", toggle_auto_wrap,{})
