2c33092138b6b5c0d3093e2b8ca9b2d4f1688955
Marios <mkatalanos@gmail.com>
2024-10-24 23:04:47 +0100

    Autoformatted, and added a new command.

--- lua/base/keybindings.lua
+++ lua/base/keybindings.lua
@@ -13,48 +13,52 @@ vim.keymap.set({ "n", "v" }, "<LocalLeader>y", [["+y]])
 -- Delete to void
 vim.keymap.set({ "n", "v" }, "<LocalLeader>d", [["_d]])
 
--- Disable Evil mode
+-- Quit all
 vim.keymap.set("n", "Q", ":qa!<CR>", { remap = true })
 
 -- Substitute current word
 vim.keymap.set("n", "<LocalLeader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- VS code style toggle wrap
-vim.keymap.set("", "<M-z>", function() vim.cmd [[set wrap!]] end)
+vim.keymap.set("", "<M-z>", function()
+	vim.cmd([[set wrap!]])
+end)
 
 -- Replace visual selection with pandoc latex
 vim.keymap.set("v", "<LocalLeader>mark", [[:'<,'>! pandoc -f markdown -t latex --biblatex<CR>]])
 
 -- Run Pandoc on Buffer
 vim.keymap.set("n", "<LocalLeader>pd", function()
-    local api = vim.api
-    -- Get the current buffer's content
-    local current_buffer = api.nvim_get_current_buf()
-    local buffer_content = api.nvim_buf_get_lines(current_buffer, 0, -1, false)
+	local api = vim.api
+	-- Get the current buffer's content
+	local current_buffer = api.nvim_get_current_buf()
+	local buffer_content = api.nvim_buf_get_lines(current_buffer, 0, -1, false)
 
-    -- Join the lines into a single string
-    local content_string = table.concat(buffer_content, "\n")
+	-- Join the lines into a single string
+	local content_string = table.concat(buffer_content, "\n")
 
-    -- Call the pandoc command on the content
-    local command = "pandoc -f markdown -t latex --biblatex"
-    local result = vim.fn.systemlist(command, content_string)
+	-- Call the pandoc command on the content
+	local command = "pandoc -f markdown -t latex --biblatex"
+	local result = vim.fn.systemlist(command, content_string)
 
+	-- Create an unnamed buffer
+	local handle = api.nvim_create_buf(false, true)
+	api.nvim_buf_set_lines(handle, 0, -1, false, result)
 
-    -- Create an unnamed buffer
-    local handle = api.nvim_create_buf(false, true)
-    api.nvim_buf_set_lines(handle, 0, -1, false, result)
-
-    -- Make a new split
-    api.nvim_command('rightbelow vertical split')
-    local window = api.nvim_get_current_win()
-    api.nvim_win_set_buf(window, handle)
+	-- Make a new split
+	api.nvim_command("rightbelow vertical split")
+	local window = api.nvim_get_current_win()
+	api.nvim_win_set_buf(window, handle)
 end)
 
 -- Term escape instead of vimode
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = 'Exit Terminal Mode' })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit Terminal Mode" })
+
+-- Term on newtab
+vim.keymap.set("n", "<leader>t", [[:tabnew +terminal<CR>i]], { desc = "Launch terminal in new tab" })
 
 -- NOHL on Escape
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Quick move to other splits
 -- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
@@ -63,6 +67,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Diagnostics
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = 'Diagnostic list' })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous Diagnostic' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next Diagnostic' })
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Diagnostic list" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
