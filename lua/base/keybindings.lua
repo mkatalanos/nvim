a78cad55f181385dfc53c6731ece870389a09e57
Marios <mkatalanos@gmail.com>
2024-11-16 15:02:04 +0000

    Made ctrl arrows move windows

--- lua/base/keybindings.lua
+++ lua/base/keybindings.lua
@@ -61,10 +61,10 @@ vim.keymap.set("n", "<leader>t", [[:tabnew +terminal<CR>]], { desc = "Launch ter
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Quick move to other splits
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Diagnostics
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Diagnostic list" })
