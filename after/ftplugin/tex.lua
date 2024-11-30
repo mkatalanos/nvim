8615ee17c768212c07a888c7c1f3e4fc0e83766f
Marios <mkatalanos@gmail.com>
2024-11-30 18:31:08 +0000

    tex ftplugin

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,15 @@
+vim.opt_local.textwidth = 100
+vim.opt_local.formatoptions = vim.opt_local.formatoptions + "t"
+
+local function toggle_auto_wrap()
+    local fo = vim.opt_local.formatoptions
+    if fo:get()["t"] then
+        print("Disabling auto-wrap")
+        vim.opt_local.formatoptions = fo - "t"
+    else
+        print("Enabling auto-wrap")
+        vim.opt_local.formatoptions = fo + "t"
+    end
+end
+
+vim.api.nvim_create_user_command("ToggleAutoWrap", toggle_auto_wrap, {})
