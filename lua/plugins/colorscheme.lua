134ba9b16c5573de2c76639da4976e79de79c3f8
Marios <mkatalanos@gmail.com>
2024-11-30 18:32:22 +0000

    Colorscheme toggle

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,66 +1,78 @@
 -- Gotham
 local Gotham = {
-    "whatyouhide/vim-gotham",
-    name = "Gotham",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("gotham256")
-    end,
+	"whatyouhide/vim-gotham",
+	name = "Gotham",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("gotham256")
+	end,
 }
 
 -- Nordtheme
 local Nord = {
-    "nordtheme/vim",
-    name = "Nord",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("nord")
-    end,
+	"nordtheme/vim",
+	name = "Nord",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("nord")
+	end,
 }
 
 -- Kanagawa
 local Kanagawa = {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("kanagawa")
-    end,
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		local dark = true
+		local switch_theme = function()
+			if dark then
+				vim.cmd.colorscheme("kanagawa-lotus")
+				dark = false
+			else
+				vim.cmd.colorscheme("kanagawa")
+				dark = true
+			end
+		end
+		vim.api.nvim_create_user_command("SwitchTheme", switch_theme, {})
+		vim.cmd.colorscheme("kanagawa")
+
+	end,
 }
 
 -- Catppuccin
 local Catppuccin = {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("catppuccin-mocha")
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
 
 -- Gruvbox
 local Gruvbox = {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        require("gruvbox")
-        vim.o.background = "dark"
-        vim.cmd.colorscheme("gruvbox")
-    end,
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require("gruvbox")
+		vim.o.background = "dark"
+		vim.cmd.colorscheme("gruvbox")
+	end,
 }
 
 -- Solarized Dark
 local Solarized = {
-    "ishan9299/nvim-solarized-lua",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("solarized")
-    end
+	"ishan9299/nvim-solarized-lua",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("solarized")
+	end,
 }
 
 return Kanagawa
